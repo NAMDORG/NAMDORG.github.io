8e32f8565a19575753519cc4a34a4c7b2a6f5809
Jacob Grodman <AvidWriter117@gmail.com>
2022-10-29 17:24:21 -0600

    Added Google Analytics

--- script.js
+++ script.js
@@ -4,20 +4,22 @@ document.addEventListener('readystatechange', event => {
 		underline();
 		moveHeader();
 		revealLinks();
+		changeLabel();
 	}
 });
 
 function underline()
 {
 	var line = document.getElementById('underline');
-	var label = document.getElementById('label');
-	var labelWidth = label.offsetWidth;
+	var label = document.getElementsByClassName('label');
+	var labelWidth = label[0].offsetWidth + label[1].offsetWidth;
 	line.style.width = labelWidth + "px";
 }
 
 function moveHeader()
 {
 	var siteHeader = document.getElementById('siteHeader');
+	
 	siteHeader.style.top = "25%";
 }
 
@@ -29,4 +31,10 @@ function revealLinks()
 	links[1].style.visibility = "visible";
 	links[0].style.transitionDelay = "3.25s";
 	links[0].style.visibility = "visible";
+}
+
+function changeLabel()
+{
+	var label = document.getElementById("label");
+	label.innerHTML = "Development";
 }
